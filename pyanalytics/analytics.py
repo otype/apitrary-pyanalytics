40949947868d97f6b4339ca603f3237ba8a00d30
Leticia Rodriguez <leti@dell6400.(none)>
2012-11-08 13:28:40 +0100

    updated analytics.py

--- pyanalytics/analytics.py
+++ pyanalytics/analytics.py
@@ -1,7 +1,5 @@
 import httplib
 import urllib
-import md5
-import os
 
 """
 
@@ -11,43 +9,30 @@ import os
 
 """
 
-def send_data (host, api_url, token_auth, urlref):
-    conn = httplib.HTTPConnection(host)
-    visitor_idstring = md5.new(os.urandom(500)).hexdigest()
-    visitor_id = visitor_idstring[:16]
-    opts = {'idsite' : '1', #  Defines the Website ID being tracked
-            'rec': '1', # Required to force the request to be recorded
-            'urlref' :  urlref, # Full referrer URL - no funciona
-            'action_name' : 'Validation page',
-            '_id' : visitor_id, #no funciona
-            'token_auth' : token_auth,
-            }
+def send_data (urlref, action_name):
+  host='111.222.111.105' # Piwik hostname
+  api_url='http://111.222.111.105/piwik/piwik.php' # Url Piwik setup
 
-        #headers = {'User-agent' : 'analytics'}
-        # make request
+  conn = httplib.HTTPConnection(host) # Open connection
+  
+  # GET parameters:
+  opts = {'idsite' : '1', #  Defines the Website ID being tracked
+               'rec': '1', # Required to force the request to be recorded
+               'urlref' :  urlref,
+               'action_name' : action_name 
+               }
 
-        # Encode this data and generate the final URL
-    data = urllib.urlencode(opts)
-    print data
-    conn.request('POST', u"%s?%s" % (api_url, data), headers={'User-Agent' : 'analyticspiwik'})
-
-        #response = conn.getresponse(urllib2.urlopen(conn.request))
-    response = conn.getresponse()
-    response.read()
-    print response.read()
-
-    print response.status, response.reason
-    conn.close()
-    return response.read()
-
-#        conn.close()
+  # Encode this data and generate request with the final URL
+  data = urllib.urlencode(opts)
+  conn.request('GET', u"%s?%s" % (api_url, data), headers={'User-Agent' : 'genapi analytics-piwik'})
+  response = conn.getresponse()
+  conn.close()
+  print data, response.status, response.reason # Debug info
+  return response.read()
 
 if __name__ == "__main__":
-    analytics = send_data(
-                          host='app1.dev.apitrary.net', # Url apitrary
-                          api_url='http://app1.dev.apitrary.net/piwik/piwik.php', #Url Piwik setup
-                          #_id='aaaa111122223333', # id user
-                          urlref="http://yahoo.comj",
-                          token_auth='1818f4d2511808845473978ff481ebe2' # User authentication API Piwik
-                          )
-    print analytics
+        analytics = send_data(
+                              urlref="http://test.apitrary.net", # Full Referrer URL. Could be used as domain-id.
+                              action_name='Validation testing page' # Page title. Could be used as api-id.
+            )
+        print analytics
