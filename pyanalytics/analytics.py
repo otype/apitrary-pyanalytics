7ed4edfdcbd50b5967adc5815e76e7cec772197a
Hans-Gunther Schmidt <hans@otype.de>
2012-11-14 17:23:04 +0100

    playing around with own analytics API

--- pyanalytics/analytics.py
+++ pyanalytics/analytics.py
@@ -1,6 +1,4 @@
-import httplib
-import urllib
-
+# -*- coding: utf-8 -*-
 """
 
     GenAPI - Analytics library
@@ -8,31 +6,32 @@ import urllib
     Copyright (c) 2012 apitrary
 
 """
+import httplib
+import urllib
 
-def send_data (urlref, action_name):
-    host='app1.dev.apitrary.net' # Piwik hostname
-    api_url='http://app1.dev.apitrary.net/piwik/piwik.php' # Url Piwik setup
+def send_data(api_id, entity_name):
+    host = '{}.dev.api.apitrary.com'.format(api_id)
+    api_url = 'http://{}.dev.api.apitrary.com/{}'.format(api_id, entity_name)
+    print api_url
 
     conn = httplib.HTTPConnection(host) # Open connection
-    
-    # GET parameters:
-    opts = {'idsite' : '1', #  Defines the Website ID being tracked
-               'rec': '1', # Required to force the request to be recorded
-               'urlref' :  urlref,
-               'action_name' : action_name 
-               }
 
-        # Encode this data and generate request with the final URL
+    # GET parameters:
+    opts = {
+        'urlref': 'test',
+        'action_name': 'test_action'
+    }
+
+    # Encode this data and generate request with the final URL
     data = urllib.urlencode(opts)
-    conn.request('GET', u"%s?%s" % (api_url, data), headers={'User-Agent' : 'genapi analytics-piwik'})
+    conn.request('POST', u"%s?%s" % (api_url, data), headers={'User-Agent': 'genapi analytics-piwik'})
     response = conn.getresponse()
     conn.close()
-    print data, response.status, response.reason # Debug info
-    return response.read()
+    return data, response.status, response.reason, response.read()
 
 if __name__ == "__main__":
     analytics = send_data(
-                         urlref="http://test.apitrary.com", # Full Referrer URL. Could be used as domain-id.
-                         action_name='11111ru4gwydgb4v585h1nfmxjuyrt' # Page title. Could be used as api-id.
-                  )
+        api_id='a8e29b20501846a7ba592f67b0accdaa',
+        entity_name='requests'
+    )
     print analytics
